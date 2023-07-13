7fed88f12a43cc2d822e2534a19922490abec7f0
Manh Nguyen <ndmanh.itc@gmail.com>
2023-07-13 08:56:41 +0000

    Add logic for required files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockernamespace=manhnguyen308
+dockerpath=project-ml-microservice
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy project-ml-microservice-kubenetes --image=$dockernamespace/$dockerpath:1.0
 
 # Step 3:
 # List kubernetes pods
+kubectl get deploy,rs,svc,pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment.apps/project-ml-microservice-kubenetes --address 0.0.0.0 5000:5000
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,26 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockernamespace=manhnguyen308
+dockerpath=project-ml-microservice
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+cat my_password.txt | docker login --username $dockernamespace --password-stdin
+image_project_ml=$(docker image list --filter=reference="$dockerpath:1.0" | grep "$dockerpath" | xargs)
+echo $image_project_ml
+
+image_info=$(docker image list | grep "$dockerpath" | xargs)
+image_tag=$(echo "$image_info" | cut -f 2 -d " ")
+image_id=$(echo "$image_info" | cut -f 3 -d " ")
+
+echo $image_id
+echo $image_tag
+
+docker image tag "$image_id" "$dockernamespace/$dockerpath:$image_tag"
+#docker image list --filter=reference="$docker_path"
 
 # Step 3:
 # Push image to a docker repository
+docker image push "$dockernamespace/$dockerpath:$image_tag"
